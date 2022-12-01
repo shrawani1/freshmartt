149be2218816d8fb3b392c5169154a185158e9d7
Shreejan Manandhar <shreejan0497@gmail.com>
2022-12-01 21:24:30 +0545

    AddToBill and Selecting Done

--- src/controller/Product_Controller.java
+++ src/controller/Product_Controller.java
@@ -4,7 +4,6 @@
  */
 package controller;
 
-
 import database.DbConnection;
 import models.Product;
 import java.sql.*;
@@ -27,26 +26,21 @@ public class Product_Controller {
         int result = dbConnection.manipulate(addProductQuery);
         return result;
     }
-    public Product fetchProduct(){
-           String selectQuery = "select * from product_table";  
-           dbConnection = new DbConnection();
+
+    public ResultSet fetchProduct(Product product) {
+        String category = product.getProductCategory();
+        String selectQuery = "select * from product_table where productCategory='" + category + "'";
+        dbConnection = new DbConnection();
         ResultSet result = dbConnection.retrieve(selectQuery);
-                    try{
-                        while(result.next()){
-            int productId = Integer.parseInt(result.getString("productId"));
-String productName= result.getString("productName");
-int productQty = Integer.parseInt(result.getString("productQty"));
-int productPrice = Integer.parseInt(result.getString("productPrice"));
-String productCategory = result.getString("productCategory");
-        Product fetchedProduct = new Product(productId,productName,productQty,productPrice,productCategory);
-        System.out.println(fetchedProduct);
-return fetchedProduct;
-                        }
-            } catch(SQLException ex){
-            ex.printStackTrace();
-            }
-        return null;
-    
+        return result;
+
+    }
+
+    public ResultSet selectDeatils(Product product){
+        String name = product.getProductName();
+        String selectQuery = "select productPrice from product_table where productName='"+name+"'";
+        dbConnection = new DbConnection();
+        ResultSet result = dbConnection.retrieve(selectQuery);
+        return result;
     }
 }
-
